9eefc33fafd02a1fc5a70bc88090d31bc66995a4
Andre Sovgir <andre.sovgir@gmail.com>
2013-06-02 18:10:55 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MRT::Application.config.secret_token = 'a6c3ef143186c84a7d7aa906e400bd9bc56ef1918d2abf73f81717a5fda7e4d0b00dae734c2c82fa4634c30688ee26930aa5cc0fddcd159704c336f672970eb6'
