fb603e0dcb1772147eec385b8b6b073c583fd59b
Andre Sovgir <andre.sovgir@gmail.com>
2014-03-03 16:18:06 -0800

    fixed images

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,7 +3,7 @@ class Movie < ActiveRecord::Base
   attr_accessible :title, :description, :avatar
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" },
   					:storage => :s3,
-  					:s3_host_name => 'moviert.s3-website-us-east-1.amazonaws.com',
+  					:s3_host_name => 's3.amazonaws.com',
 					 :bucket => 'moviert',
 					 :aws_credentials => "#{Rails.root}/config/aws.yml"
   has_many :explanations
---
+++ config/initializers/aws-sdk.rb
@@ -0,0 +1,4 @@
+AWS.config({
+  :access_key_id => 'AKIAIIJG3RNQQD3G4BZA',
+  :secret_access_key => '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',
+})
