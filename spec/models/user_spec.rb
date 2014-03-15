13fcb64f8690fa1fab3b8300c5d1abd53b039b5e
Chris Yin <ceyin.818@gmail.com>
2014-03-14 21:11:41 -0700

    finish 6.2.4, add email format validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -22,4 +22,30 @@ describe User do
   	before { @user.email = " " }
   	it { should_not be_valid }
   end
+
+  describe "when username is too long" do
+  	before { @user.email = "a" * 51 }
+  	it { should_not be_valid }
+  end
+
+  describe "when email format is invalid" do
+  	it "should be invalid" do
+  	  addresses = %w[user@foo,com user_at_foo.org example.user@foo.
+  	                 foo@bar_bz.com foo@bar+baz.com]
+  	  addresses.each do |invalid_address|
+  	    @user.email = invalid_address
+  	    expect(@user).not_to be_valid
+  	  end
+  	end
+  end
+
+  describe "when email format is valid" do
+  	it "should be valid" do
+  	  addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+  	  addresses.each do |valid_adddress|
+  	  	@user.email = valid_adddress
+  	  	expect(@user).to be_valid
+  	  end
+  	end
+  end
 end
