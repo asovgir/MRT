12a68f7e393a028842ec83ce3d0ce81fb436e353
Chris Yin <ceyin.818@gmail.com>
2014-03-15 14:24:08 -0700

    finishied 7.2

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 gem 'rails', '3.2.13'
 gem 'bcrypt-ruby', '~> 3.0.0'
 gem 'devise'
+gem 'bootstrap-sass', '2.3.2.0'
 
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
--- config/application.rb
+++ config/application.rb
@@ -62,5 +62,6 @@ module MRT
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     config.serve_static_assets = true
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end
