5ce4acdf00e8205a06be103bf7244780d1622dfa
Chris Yin <ceyin.818@gmail.com>
2014-03-14 20:45:57 -0700

    finish 6.2.1

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140201035206) do
+ActiveRecord::Schema.define(:version => 20140315023854) do
 
   create_table "explanations", :force => true do |t|
     t.integer  "movie_id"
     t.string   "synopsis"
-    t.text     "explanation" , :limit => nil
+    t.string   "explanation"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
   end
@@ -29,7 +29,14 @@ ActiveRecord::Schema.define(:version => 20140201035206) do
     t.string   "avatar_file_name"
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
-    t.datetime "avatar_updated_at" 
+    t.datetime "avatar_updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
