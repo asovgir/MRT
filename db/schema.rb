12bef82e3581017b2c75a236db2c411f93385f78
Chris Yin <ceyin.818@gmail.com>
2014-03-14 21:25:05 -0700

    finish all 6.2, email uniqueness

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140315023854) do
+ActiveRecord::Schema.define(:version => 20140315042120) do
 
   create_table "explanations", :force => true do |t|
     t.integer  "movie_id"
@@ -38,5 +38,7 @@ ActiveRecord::Schema.define(:version => 20140315023854) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
