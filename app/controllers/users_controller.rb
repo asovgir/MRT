06a3ef2bfd37bb635d84744c29d82e1d2153291a
Chris Yin <ceyin.818@gmail.com>
2014-03-15 14:41:48 -0700

    finish 7.3.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,19 @@ class UsersController < ApplicationController
   def new
   	@user = User.new
   end
+
+  def create
+  	@user = User.new(params[:user])
+  	if @user.save
+  	else
+  	  render 'new'
+  	end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, 
+      	                           :password_confirmation)
+    end
 end
