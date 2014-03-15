18ee0ee59117877c0264a69b74fccf0ea06596fc
Chris Yin <ceyin.818@gmail.com>
2014-03-15 01:05:33 -0700

    at listing 7.10, specs failing

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+  	name                  "Chris Yin"
+  	email                 "ceyin.818@gmail.com"
+  	password              "foobar"
+  	password_confirmation "foobar"
+  end
+end
